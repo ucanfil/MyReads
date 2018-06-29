79f358a0106f4fa0da2133d380c332645ecf8055
Burak Tilek <burak.tilek@gmail.com>
2018-06-29 17:41:28 +0300

    Initial commit of making project dynamic

---
+++ src/BookShelf.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Books from './Books'
+
+class BookShelf extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default BookShelf
