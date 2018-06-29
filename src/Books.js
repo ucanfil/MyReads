9ab6362731454a9d6aaf222a42c4c018231dd096
Burak Tilek <burak.tilek@gmail.com>
2018-06-30 02:30:41 +0300

    Book compenent implemented

--- src/Books.js
+++ src/Books.js
@@ -1,59 +1,36 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 class Books extends Component {
+
   render() {
-    return (
-      <ol className="books-grid">
-        {this.props.books.map(book => (
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="move" disabled>
-                      Move to...
-                    </option>
-                    <option value="currentlyReading">
-                      Currently Reading
-                    </option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
+    return <div className="bookshelf-books">
+        <ol className="books-grid">
+          {this.props.books.map(book => <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
+                  <div className="book-shelf-changer">
+                    <select onChange={event => this.props.onChangeShelf(book, event.target.value)}>
+                      <option value="move" disabled>
+                        Move to...
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
                 </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors[0]}</div>
+                <div className="book-authors">{book.authors[1]}</div>
               </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors[0]}</div>
-              <div className="book-authors">{book.authors[1]}</div>
-            </div>
-          </li>
-        ))}
-        <li>
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }} />
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="move" disabled>
-                    Move to...
-                  </option>
-                  <option value="currentlyReading">
-                    Currently Reading
-                  </option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
-            </div>
-            <div className="book-title">Ender's Game</div>
-            <div className="book-authors">Orson Scott Card</div>
-          </div>
-        </li>
-      </ol>
-    )
+            </li>)}
+        </ol>
+      </div>;
   }
 }
 
